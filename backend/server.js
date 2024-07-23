641c55979f87ca7c0a4d478e9e1d84a52c8b5a44
kingskiller <93809757+kingskiller7@users.noreply.github.com>
2024-07-24 00:29:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ dotenv.config();
 const app = express();
 const cors = require('cors');
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'https://books-frontend-lemon.vercel.app',
-    // origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: 'https://books-frontend-lemon.vercel.app',
+//     // origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
 
 const url = process.env.MONGODB_URL;
 mongoose.connect(url);
