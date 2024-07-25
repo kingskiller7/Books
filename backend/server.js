76e46992908e3f4325c8c5fa64ecb78e264bbeb0
kingskiller <93809757+kingskiller7@users.noreply.github.com>
2024-07-25 23:58:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,12 @@ const bookController = require('./controller/books');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const cors = require('cors');
+const http = require('http');
 
 dotenv.config();
 const app = express();
-// app.use(cors());
 app.use(cors({
     origin: 'https://books-home.vercel.app',
-    // origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
@@ -30,3 +29,11 @@ app.put('/books/:id', bookController.updateBook);
 app.delete('/books/:id', bookController.deleteBook);
 
 app.listen(5000, () => console.log('Server running on port 5000'));
+
+setInterval(() => {
+  http.get('https://books-ctyk.onrender.com:5000', (res) => {
+    console.log('Pinging server to keep it alive...');
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`);
+  });
+}, 300000);
