e9e3efad5b1eba83b8e354ce9da22e29353ee8be
kingskiller <93809757+kingskiller7@users.noreply.github.com>
2024-07-25 23:58:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookList from './components/bookList';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-const http = require('http');
-
-setInterval(() => {
-  http.get('https://books-ctyk.onrender.com:5000', (res) => {
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-    res.on('end', () => {
-      console.log('No more data in response.');
-    });
-  }).on('error', (e) => {
-    console.log(`Got error: ${e.message}`);
-  });
-}, 300000);
 
 
 function App() {
